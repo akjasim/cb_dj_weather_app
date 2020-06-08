8f1d73b4c0b9415f6b85642dd4b06525aa93717e
akjasim <jasimjasim786@gmail.com>
2020-06-08 11:26:11 +0530

    complete logic

---
+++ core/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def get_html_content(request):
+    import requests
+    city = request.GET.get('city')
+    city = city.replace(" ", "+")
+    USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+    LANGUAGE = "en-US,en;q=0.5"
+    session = requests.Session()
+    session.headers['User-Agent'] = USER_AGENT
+    session.headers['Accept-Language'] = LANGUAGE
+    session.headers['Content-Language'] = LANGUAGE
+    html_content = session.get(f'https://www.google.com/search?q=weather+{city}').text
+    return html_content
+
+
+def home(request):
+    result = None
+    if 'city' in request.GET:
+        # fetch the weather from Google.
+        html_content = get_html_content(request)
+        from bs4 import BeautifulSoup
+        soup = BeautifulSoup(html_content, 'html.parser')
+        result = dict()
+        # extract region
+        result['region'] = soup.find("div", attrs={"id": "wob_loc"}).text
+        # extract temperature now
+        result['temp_now'] = soup.find("span", attrs={"id": "wob_tm"}).text
+        # get the day and hour now
+        result['dayhour'] = soup.find("div", attrs={"id": "wob_dts"}).text
+        # get the actual weather
+        result['weather_now'] = soup.find("span", attrs={"id": "wob_dc"}).text
+    return render(request, 'core/home.html', {'result': result})
